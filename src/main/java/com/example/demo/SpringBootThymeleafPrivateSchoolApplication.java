66cc5d243abb45edb26a3c00b6391b9922786dd6
biseras <biserasss01@gmail.com>
2022-04-01 00:09:31 +0200

    initialcommit

---
+++ src/main/java/com/example/demo/SpringBootThymeleafPrivateSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootThymeleafPrivateSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootThymeleafPrivateSchoolApplication.class, args);
+    }
+
+}
